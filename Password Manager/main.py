38f1cea92853355d8fa03998711a5e272d2c331f
Virat-Gupta <Virat_gupta@outlook.com>
2024-08-10 00:23:45 +0530

    Updated password Manager

--- Password Manager/main.py
+++ Password Manager/main.py
@@ -2,7 +2,25 @@ from tkinter import * # type: ignore
 from tkinter import messagebox
 from random import randint,choice,shuffle
 import pyperclip
+import json
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+def find_password():
+    web = websiteBox.get()
+    try:
+        with open("Password Manager\\data.json","r") as passFile :
+            data = json.load(passFile)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No data File Found")
+    else:
+        if (web in data):
+            user = data[web]["email"]
+            passwd = data[web]["password"]
+            messagebox.showinfo(title=web, message=f"Email/User: {user} \n Password: {passwd}") 
+        else :
+            messagebox.showinfo(title="Error", message=f"No details for {web} exists")
+            
+
 #Password Generator Project
 def generate_password():
     password.delete(0,END)
@@ -27,17 +45,33 @@ def addPassword():
     web = websiteBox.get()
     user = username.get()
     passwd = password.get()
+    new_data = {
+        web: {
+            "email" : user,
+            "password" : passwd,
+        }
+    }
 
     if web == "" or user == "" or passwd == "":
         messagebox.showinfo(title="ERROR", message="Please Dont Leave Feilds Empty")
-        return
-
-    isOk = messagebox.askokcancel(title=web, message=f"These are the details entered: \n Email: {user} \n Password : {passwd} \n Is this correct ?")
-    if isOk :
-        with open("Password Manager\\data.txt","a") as passFile:
-            passFile.write(f"{web} | {user} | {passwd}\n")
-        websiteBox.delete(0,END)
-        password.delete(0,END)
+        # return
+    # isOk = messagebox.askokcancel(title=web, message=f"These are the details entered: \n Email: {user} \n Password : {passwd} \n Is this correct ?")
+    # if isOk :
+    else:
+        try:
+            with open("Password Manager\\data.json","r") as passFile:
+                data = json.load(passFile)
+        except FileNotFoundError:
+            with open("Password Manager\\data.json","w") as passFile:
+                json.dump(new_data, passFile, indent=4)
+        else :
+            data.update(new_data)
+            with open("Password Manager\\data.json","w") as passFile:
+                # passFile.write(f"{web} | {user} | {passwd}\n")
+                json.dump(data,passFile, indent=4)
+        finally:
+            websiteBox.delete(0,END)
+            password.delete(0,END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -51,9 +85,11 @@ canvas.grid(row=0,column=1)
 
 websiteLabel = Label(text="Website :")
 websiteLabel.grid(row=1,column=0)
-websiteBox = Entry(width=35)
+websiteBox = Entry(width=21)
 websiteBox.focus()
-websiteBox.grid(row=1, column=1, columnspan=2, sticky="ew")
+websiteBox.grid(row=1, column=1, sticky="ew")
+seachButton = Button(text="Search", command=find_password)
+seachButton.grid(row=1, column=2, sticky="ew")
 
 usernameLabel = Label(text="Email/Username :")
 usernameLabel.grid(row=2,column=0)
